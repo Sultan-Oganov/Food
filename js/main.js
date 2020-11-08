c5e0ff1e57c618aeafa137182936b78292076517
Sultan <sultan.oganov@gmail.com>
2020-11-08 22:45:05 +0600

    requesting form data using fetch in JSON format

--- js/main.js
+++ js/main.js
@@ -246,20 +246,19 @@ window.addEventListener('DOMContentLoaded', () =>{
 
 			const formData = new FormData(form);
 
-			// const object = {};
-			// formData.forEach((value, key) => {
-			// 	object[key] = value;
-			// });
-
-			// const json = JSON.stringify(object);
+			const object = {};
+			formData.forEach((value, key) => {
+				object[key] = value;
+			});
 
 			fetch('server.php', {
 				method: 'POST',
-				// headers: {
-				// 	'Content-type': 'application/json'
-				// },
-				body: formData
-			}).then(data => data.text())
+				headers: {
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(object)
+			})
+			.then(data => data.text())
 			.then(data => {
 					console.log(data);
 					showThanksModal(message.success);
